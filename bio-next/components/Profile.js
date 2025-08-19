2f27eab1b749fac153e9e8b8087617ab8ed63654
ygortsf <ygordetarcio@gmail.com>
2025-08-19 15:29:57 +0000

    agora vai

--- bio-next/components/Profile.js
+++ bio-next/components/Profile.js
@@ -2,7 +2,7 @@ export function Profile(){
     return(
         <>
         <img
-        src="https://instagram.frec52-1.fna.fbcdn.net/v/t51.2885-19/495492715_17904178221169829_8515611092847156175_n.jpg?stp=dst-jpg_s150x150_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLmRqYW5nby4xMDgwLmMyIn0&_nc_ht=instagram.frec52-1.fna.fbcdn.net&_nc_cat=105&_nc_oc=Q6cZ2QHWRWMmD62Gh1XaLw4tnAH9vPH3TfQSmCupYjr7U3HB2k29OeqQXaGQRMom2KKv-Dw8iAPaWrVpIQx6WeH_-8a2&_nc_ohc=k4dUFKsn66MQ7kNvwG6QBEs&_nc_gid=luHpWCIZNrrIFRvtLaNBdQ&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfVv1Exv2nhD4ySmHk3Pj6_zvSuuc-QXRxeCNfsCniOq5g&oe=68AA6712&_nc_sid=8b3546"
+        scr="https://media-for2-2.cdn.whatsapp.net/v/t61.24694-24/517408917_1438649517417679_3058450668514200468_n.jpg?ccb=11-4&oh=01_Q5Aa2QFPe47x8jt56vqVRPXnYfY-RHINOozqELhEz5cJQkjqag&oe=68B1A827&_nc_sid=5e03e0&_nc_cat=108"
         alt="Ygor de Tarcio"
         class="photo"
         />
