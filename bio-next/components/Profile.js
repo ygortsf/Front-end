4e8395cfd2ac258318900ce6c81e0d3244c95219
ygortsf <ygordetarcio@gmail.com>
2025-08-20 02:32:44 +0000

    hahay

--- bio-next/components/Profile.js
+++ bio-next/components/Profile.js
@@ -5,9 +5,9 @@ export function Profile(){
         src="/minha-foto.jpg"
         alt="Ygor de Tarcio"
         class="photo"
-        width={300}
-        height={300}
+        width={200}
         />
+        <hr/>
         <h1> Ygor de Tárcio</h1>
 
         <p>Me chamo Ygor de Tárcio, tenho 19 anos e sou Aluno da disciplina Programação Front-End na Unicap, localizada no bairro da Boa Vista, Recife</p>
